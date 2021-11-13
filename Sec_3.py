a67721509f589d2b30bdade937fe4fff5f3e468a
SantiagoCL <72629391+SantiagoCL@users.noreply.github.com>
2021-11-13 16:04:44 -0300

    Update Sec_3.py

--- Sec_3.py
+++ Sec_3.py
@@ -1,124 +1,147 @@
-'''Ecuaciones de la seccion 2 de ASCE - 8 - 02
-
-'''
-
 from math import pi
 import numpy as np
 
-class  sec2_1_1():
-    '''Section 2.1.1: Dimensional Limits and Considerations.
+def E3_3_1_2_e6(E0, K, L, r, eta):
+    '''s_ex en Eq 3.3.1.2-6. Tension critica de pandeo flexional
 
     # Parametros
-    profile: Dimensiones del perfil
-    stiffCondition: Rigidización de los elementos 'UNSTIFFNED' (iii) | STIFFNED_SL (i) | STIFFNED (ii)
-
-    '''
-    #valores predefinidos
-    def __init__(self, member, stiffCondition = 'UNSTIFFNED'):
-        self.Name = 'Sec2.1.1'
-        self.w = member.profile.w
-        self.wf = member.profile.w
-        self.t = member.profile.t
-        self.L = member.L
-        self.stiffCondition = stiffCondition
-
-    def Cl_1(self):
-        ''' Determina la relación máxima entre el ancho del ala y su esperor. Los valores limites se dan para tres condiciones de rigidización
-        '''
-        if self.stiffCondition == 'UNSTIFFNED':
-            ratioAdm = 50.0 # 2.1.1-1.iii
-            ratio = self.w/self.t
-            print ('Esbeltez =',ratio, '<','Esbeltez admisible =', ratioAdm)
-            return ratio < ratioAdm
-        elif self.stiffCondition == 'STIFFNED':
-            ratioAdm = 400.0 # 2.1.1-1.ii
-            ratio = self.w/self.t
-            print (ratio, '<', ratioAdm)
-            return ratio < ratioAdm
-        elif self.stiffCondition == 'STIFFNED_SL':
-            ratioAdm = 90.0 # 2.1.1-1.ii
-            ratio = self.w/self.t
-            print (ratio, '<', ratioAdm)
-            return ratio < ratioAdm
-    def Cl_2(self):
-        raise 'Not implemented'
-    def Cl_3(self):
-        '''Especifica el ancho efectivo en el caso de vigas cortas soportando una carga puntual.
-        '''
-        if self.L < 30*self.wf:
-            self.ratio = self.TABLE1()
-        else:
-            self.ratio = 1.0
-        self.w_eff = self.w*self.ratio
-    def TABLE1(self):
-        '''TABLE 1. Short, Wide Flanges: Maximum Allowable Ratio of Effective Design Width to Actual Width
-
-        # Tests
-
-        >>> TABLE1()
-        '''
-        table1 = np.array(((30, 1.00),
-                (25, 0.96),
-                (20, 0.91),
-                (18, 0.89),
-                (16, 0.86),
-                (14, 0.82),
-                (12, 0.78),
-                (10, 0.73),
-                (8, 0.67),
-                (6, 0.55),
-        ))
-        r = np.interp( table1[:,0], table1[:,1], self.L/self.wf )
-        return r
-########################### USO
-'''#INPUT:
-flangeCURLING = 'False'
-shearLag = 'True'
-# VERIFICACION
-for member in members:
-    sec2_1_1 = sec2_1_1(memberProperties(member))
-    print ('Los elementos del miembro', member, 'cumplen con las dimensiones requeridas:', sec2_1_1.Cl_1())
-    if flangeCURLING:
-        sec2_1_1.Cl_2
-    if shearLag:
-        sec2_1_1.Cl_3'''
-
-def E_5_5_1(E0,K,L):
-    '''Calcula la carga critica de euler para el caso stiffned
-
-    # Tests
-    >>> round( E_5_5_1(186200, 1.5, 200) ,2)
-    20.42
-
-    '''
-    Pcr = pi**2* E0 / (K*L)**2
-    return Pcr
-
-def E_5_5_2(E0,K,L):
-    '''Calcula la carga critica de euler para el caso unstiffned
-
-    # Parametros
-    E0: Modulo elasticidad incial
+    E0: Modulo elasticidad
     K: Factor de longitud efectiva
-    L: Longitud del miembro
+    L: longitud del miembro
+    r: radio de giro del miembro | sqrt(I/A)
+    eta: factor de reduccion plastica | Et(s)/E0
 
     # Tests
-    >>> round( E_5_5_2(E0 = 186200, K = 1.5, L = 200) ,2)
-    10.21
-
+    >>> round (E3_3_1_2_e6(E0 = 180510 ,K = 0.5,L = 1800, r = 40.272 , eta = 0.6225), 2)
+    2220.56
     '''
-    Pcr = pi**2* E0 / (K*L)**2*0.5
-    return Pcr
 
-def E_2_5(E0, K, L):
-    '''Carga de pandeo lateral-torsional.
+    den = (K*L/r)**2
+    s_ex = pi**2*E0 / den * eta
+    return s_ex
+
+def E3_4_2_e1(E0,Kt,Lt, rx, ry, eta, c_x, sc_x, A, Cw, G0, J):
+    '''Fn = s_t en Eq 3.4.2-1. Tension critica de Torsional
+    
+    
+    # Parametros
+    E0: Modulo elasticidad
+    Kt: Factor de longitud efectiva a torsion
+    Lt: longitud del miembro a torsion
+    rx, ry: radio de giro del miembro | sqrt(I/A)
+    eta: factor de reduccion plastica | Et(s)/E0
+    c_x: coordenada del centroide de la seccion
+    sc_x: coordenada del centro de corte
+    A: Area de la seccion
+    Cw: Constante torsional de warping de la seccion
+    G0: Modulo de corte inicial
+    J: Constante de torsion de St. Venant  
+
+    # Tests
+    >>> round ( E3_4_2_e1(E0 = 180510, Kt = 0.5, Lt = 1800, rx = 40.272, ry = 18.2673, eta = 0.6225, c_x = 15.59, sc_x = 23.1, A = 319, Cw = 215e6, G0 = 69426.9, J = 239), 2)
+    276.66
+    '''
+    Fn = E3_3_1_2_e8(E0,Kt,Lt, rx, ry, eta, c_x, sc_x, A, Cw, G0, J)
+    return Fn
+
+def E3_4_3_e1(E0, Kx, Lx, Kt,Lt, rx, ry, eta, c_x, sc_x, A, Cw, G0, J):
+    '''Fn = en Eq 3.4.3-1. Tension critica de pandeo Flexo-torsional
+
 
     # Parametros
+    E0: Modulo elasticidad
+    Kt: Factor de longitud efectiva a torsion
+    Lt: longitud del miembro a torsion
+    rx, ry: radio de giro del miembro | sqrt(I/A)
+    eta: factor de reduccion plastica | Et(s)/E0
+    c_x: coordenada del centroide de la seccion
+    sc_x: coordenada del centro de corte
+    A: Area de la seccion
+    Cw: Constante torsional de warping de la seccion
+    G0: Modulo de corte inicial
+    J: Constante de torsion de St. Venant  
 
     # Tests
-    >>> round( E_2_5(E0 = 186200, K = 1.5, L = 200) ,2)
-    20.42
-
+    >>> round ( E3_4_3_e1(E0 = 180510, Kx = 0.5, Lx = 1800, Kt = 0.5, Lt = 1800, rx = 40.272, ry = 18.2673, eta = 0.6225, c_x = 15.59, sc_x = 23.1, A = 319, Cw = 215e6, G0 = 69426.9, J = 239), 2)
+    261.53
     '''
-    LTB = pi**2* E0 / (K*L)**2
-    return LTB
+
+    x0 = -(c_x+sc_x) # distancia desde el centroide al centro de corte, negativo
+    r0 = E3_3_1_2_e9(rx, ry, x0) # radio de giro polar
+    beta = 1- (x0/r0)**2
+    
+    t1 = eta/2/beta
+    s_ex = E3_3_1_2_e6(E0, Kx, Lx, rx, 1.0)
+    s_t = E3_3_1_2_e8(E0,Kt,Lt, rx, ry, 1.0, c_x, sc_x, A, Cw, G0, J)
+    raiz = ( (s_ex + s_t)**2 - 4*beta*s_ex*s_t )**0.5
+    Fn = t1*(s_ex + s_t - raiz)
+    return Fn
+
+
+def E3_4_3_e3(E0,K,L,r,eta):
+    '''s_ex en Eq 3.4.3-3.
+
+    # Parametros
+    E0: Modulo elasticidad
+    K: Factor de longitud efectiva
+    L: longitud del miembro
+    r: radio de giro del miembro | sqrt(I/A)
+    eta: factor de reduccion plastica | Et(s)/E0
+
+    # Tests
+    >>> round (E3_4_3_e3(E0 = 180510 ,K = 0.5,L = 1800, r = 40.272 , eta = 0.6225), 2)
+    2220.56
+    '''
+
+    s_ex = E3_3_1_2_e6(E0,K,L,r,eta)
+    return s_ex
+
+def E3_3_1_2_e8(E0,Kt,Lt, rx, ry, eta, c_x, sc_x, A, Cw, G0, J):
+    '''s_t en Eq 3.3.1.2-6.
+
+    Ver parametros de seccion en https://sectionproperties.readthedocs.io/en/latest/rst/post.html
+
+    # Parametros
+    E0: Modulo elasticidad
+    Kt: Factor de longitud efectiva a torsion
+    Lt: longitud del miembro a torsion
+    rx, ry: radio de giro del miembro | sqrt(I/A)
+    eta: factor de reduccion plastica | Et(s)/E0
+    c_x: coordenada del centroide de la seccion
+    sc_x: coordenada del centro de corte
+    A: Area de la seccion
+    Cw: Constante torsional de warping de la seccion
+    G0: Modulo de corte inicial
+    J: Constante de torsion de St. Venant  
+
+    # Tests
+    >>> round ( E3_3_1_2_e8(E0 = 180510, Kt = 0.5, Lt = 1800, rx = 40.272, ry = 18.2673, eta = 0.6225, c_x = 15.59, sc_x = 23.1, A = 319, Cw = 215e6, G0 = 69426.9, J = 239), 2)
+    276.66
+    '''
+    x0 = -(c_x+sc_x)
+    r0 = E3_3_1_2_e9(rx, ry, x0)
+
+    t1 = 1/A/r0/r0
+    t2_den = (Kt*Lt)**2
+    t2 = G0*J + pi**2*E0*Cw/t2_den
+
+    s_t = t1*t2*eta
+    return s_t
+
+
+
+def E3_3_1_2_e9(rx,ry,x0):
+    '''r0 en Eq 3.3.1.2-8. Radio de giro polar
+
+    # Parametros\n
+    rx, ry: radios de giro\n
+    x0: distancia desde el centro de corte al centroide, en valor negativo\n
+
+    # Tests\n
+    >>> round( E3_3_1_2_e9(40.272,18.2673,38.69), 2)
+    58.76
+    '''
+    r0 = (rx**2 + ry**2 + x0**2)**0.5
+    return r0
+
+
